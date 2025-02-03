0a929974f0534067e32b5063c1610d5321ea5968
Yuto Murakoshi <yuto.murakoshi@office-asot.com>
2025-02-03 17:48:00 +0900

    メニュー周りのCSSを修正

--- app/components/side-menu.tsx
+++ app/components/side-menu.tsx
@@ -6,36 +6,36 @@ export default function SideMenu({ isMenuOpen, closeMenu }) {
   return (
     <aside className={`${styles.sideMenu} ${isMenuOpen ? styles.open : ""}`}>
       <ul className={styles.sideMenuList}>
-        <Link to="/" onClick={closeMenu}>
-          <li className={styles.sideMenuItem}>
+        <li className={styles.sideMenuItem}>
+          <Link to="/" onClick={closeMenu}>
             <span className={`material-icons ${styles.sideMenuItemIcon}`}>home</span>
             <span className={styles.sideMenuItemText}>ホーム</span>
-          </li>
-        </Link>
-        <Link to="/graph" onClick={closeMenu}>
-          <li className={styles.sideMenuItem}>
+          </Link>
+        </li>
+        <li className={styles.sideMenuItem}>
+          <Link to="/graph" onClick={closeMenu}>
             <span className={`material-icons ${styles.sideMenuItemIcon}`}>query_stats</span>
             <span className={styles.sideMenuItemText}>グラフ</span>
-          </li>
-        </Link>
-        <Link to="/farm-field" onClick={closeMenu}>
-          <li className={styles.sideMenuItem}>
+          </Link>
+        </li>
+        <li className={styles.sideMenuItem}>
+          <Link to="/farm-field" onClick={closeMenu}>
             <span className={`material-icons ${styles.sideMenuItemIcon}`}>gite</span>
             <span className={styles.sideMenuItemText}>圃場</span>
-          </li>
-        </Link>
-        <Link to="/warning-email" onClick={closeMenu}>
-          <li className={styles.sideMenuItem}>
+          </Link>
+        </li>
+        <li className={styles.sideMenuItem}>
+          <Link to="/warning-email" onClick={closeMenu}>
             <span className={`material-icons ${styles.sideMenuItemIcon}`}>mail</span>
             <span className={styles.sideMenuItemText}>警報メール</span>
-          </li>
-        </Link>
-        <Link to="/fuel-order" onClick={closeMenu}>
-          <li className={styles.sideMenuItem}>
+          </Link>
+        </li>
+        <li className={styles.sideMenuItem}>
+          <Link to="/fuel-order" onClick={closeMenu}>
             <span className={`material-icons ${styles.sideMenuItemIcon}`}>local_gas_station</span>
             <span className={styles.sideMenuItemText}>燃料発注</span>
-          </li>
-        </Link>
+          </Link>
+        </li>
       </ul>
     </aside>
   );
--- app/components/top-menu.tsx
+++ app/components/top-menu.tsx
@@ -3,12 +3,10 @@ import styles from "./top-menu.module.css";
 export default function TopMenu({ toggleMenu }) {
   return (
     <nav className={styles.topMenu}>
-      <div className={styles.topMenuTitle}>サンプルアプリ</div>
-      <div className={styles.topMenuRight}>
+      <div>サンプルアプリ</div>
+      <div>
         {/* PC時はメニューを横並びで表示してもOKだが、ここでは最小構成としてボタンのみ */}
-        <button className={styles.menuButton} onClick={toggleMenu}>
-          ☰
-        </button>
+        <button className={styles.menuButton} onClick={toggleMenu}>☰</button>
       </div>
     </nav>
   );
